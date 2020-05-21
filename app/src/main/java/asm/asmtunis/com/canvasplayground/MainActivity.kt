64c8db84659cbe4f9f7510741389c117b5ca8656
gloooooory <glo8ory@gmail.com>
2020-05-21 03:34:12 +0100

    first commit

---
+++ app/src/main/java/asm/asmtunis/com/canvasplayground/MainActivity.kt
@@ -0,0 +1,17 @@
+package asm.asmtunis.com.canvasplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+    }
+}
---
+++ app/src/main/java/asm/asmtunis/com/canvasplayground/MyCanvasView.kt
@@ -0,0 +1,92 @@
+package asm.asmtunis.com.canvasplayground
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import androidx.core.content.res.ResourcesCompat
+private const val STROKE_WIDTH = 12f // has to be float
+
+class MyCanvasView(context: Context) : View(context) {
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private var path = Path()
+    private val paint = Paint().apply {
+        color = drawColor
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove(){
+    val dx = Math.abs(motionTouchEventX - currentX)
+    val dy = Math.abs(motionTouchEventY - currentY)
+    if (dx >= touchTolerance || dy >= touchTolerance) {
+        // QuadTo() adds a quadratic bezier from the last point,
+        // approaching control point (x1,y1), and ending at (x2,y2).
+        path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+        // Draw the path in the extra bitmap to cache it.
+        extraCanvas.drawPath(path, paint)
+    }
+    invalidate()
+    }
+
+
+    private fun touchUp() {
+        path.reset()
+    }
+}
